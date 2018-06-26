1d1ddbab3e61584c3d3ddb7523505f7cb8a9c1f5
lilysinger <lilymsinger@gmail.com>
2018-06-26 15:06:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ $(document).ready(function (){
             url: 'https://api.giphy.com/v1/gifs/search',
             dataType: 'json',
             data: {
-              api_key: '6ed527871fd940718d7913b1cf4751f6',
+            ',
               q: input ,
               limit: none ,
             },
