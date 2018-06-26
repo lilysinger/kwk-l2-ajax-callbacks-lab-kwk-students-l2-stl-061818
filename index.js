1317baf40e5c40ab42a2cbcd1a3d72f332294c8b
lilysinger <lilymsinger@gmail.com>
2018-06-26 14:56:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function (){
-  $.ajax ({url: "demo_test.txt", success: function(result){
+  $.ajax ({url: 'https://api.github.com/users/whatever?client_id=xxxx&client_secret=yyyy', success: function(result){
         $("#div1").html(result);
  }});
 });
