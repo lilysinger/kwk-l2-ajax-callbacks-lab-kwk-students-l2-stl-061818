9a47b9c64dfbb9884aed4e67ba89568194e7fb80
lilysinger <lilymsinger@gmail.com>
2018-06-26 14:57:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function (){
   $.ajax ({url: 'https://api.github.com/users/whatever?client_id=xxxx&client_secret=yyyy', success: function(result){
-        $("#div1").html(result);
+        $("#").html(result);
  }});
 });
